dada6f50552d4a4858bb35fde52bdc2b0f85ef82
glenn <70140106+ausgore@users.noreply.github.com>
2024-07-15 17:15:35 +0800

    Example of fetching event roles

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,22 @@ import ContactManager from "../../utils/managers/ContactManager";
 import CustomFieldSetManager, { CustomField } from "../../utils/managers/CustomFieldSetManager";
 import config from "../../../config";
 import Wrapper from "../components/Wrapper";
+import { Contact } from "../../utils/classes/Contact";
 
 export default function Home() {
+    const [contact, setContact] = useState<Contact>();
+    const email = (window as any).email ?? config.email;
+    
+    useEffect(() => {
+        (async () => {
+            const contact = await ContactManager.fetch(email);
+            setContact(contact);
+            const registeredEventRoles = await contact.fetchRegisteredEventRoles();
+            console.log(registeredEventRoles[0]["status_id:name"]);
+            console.log(registeredEventRoles[0].eventRole?.event["status_id:name"]);
+        })();
+    }, []);
+
     return <Wrapper>
         <div>
             <h1>Testing 123.</h1>
