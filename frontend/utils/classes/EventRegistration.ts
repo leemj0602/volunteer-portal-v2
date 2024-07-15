bebad4514d793e582445aa74788ded7501b01f9d
glenn <70140106+ausgore@users.noreply.github.com>
2024-07-15 17:14:53 +0800

    EventRegistration class

--- frontend/utils/classes/EventRegistration.ts
+++ frontend/utils/classes/EventRegistration.ts
@@ -1,3 +1,5 @@
+import { EventRole, EventRoleProps } from "./EventRole";
+
 export enum RegistrationStatus {
     Approved = "Completed",
     ApprovalRequired = "Approval Required",
@@ -7,17 +9,25 @@ export enum RegistrationStatus {
 export interface EventRegistrationProps {
     id: number;
     "status_id:name": RegistrationStatus;
-    "contact.email_primary.email": string;
+    [key: string]: any;
 }
 
 export class EventRegistration implements EventRegistrationProps {
     public id: number;
+    public eventRole: EventRole | null = null;
     public "status_id:name": RegistrationStatus;
-    public "contact.email_primary.email": string;
+    [key: string]: any;
     
     constructor(props: EventRegistrationProps) {
         this.id = props.id;
         this["status_id:name"] = props["status_id:name"];
-        this["contact.email_primary.email"] = props["contact.email_primary.email"];
+
+        const eventRoleDetails: Partial<EventRoleProps> = {};
+        for (const key in props) {
+            if (key.startsWith("eventRole.")) eventRoleDetails[key.split("eventRole.")[1]] = props[key];
+            if (key.startsWith("event.")) eventRoleDetails[key] = props[key];
+        }
+
+        this.eventRole = new EventRole(eventRoleDetails as EventRoleProps);
     }
 }
