c186888159c592169b08b5ac1e3228603c547df9
glenn <70140106+ausgore@users.noreply.github.com>
2024-07-17 16:09:27 +0800

    EventRegistration.attendance

--- frontend/utils/classes/EventRegistration.ts
+++ frontend/utils/classes/EventRegistration.ts
@@ -1,5 +1,19 @@
 import { EventRole, EventRoleProps } from "./EventRole";
 
+interface EventAttendanceProps {
+    id: number;
+    duration: number | null;
+    [key: string]: any;
+}
+class EventAttendance implements EventAttendanceProps {
+    public id: number;
+    public duration: number | null = null;
+    constructor(props: EventAttendanceProps) {
+        this.id = props.id;
+        this.duration = props.duration;
+    }
+}
+
 export enum RegistrationStatus {
     Approved = "Completed",
     ApprovalRequired = "Approval Required",
@@ -15,7 +29,8 @@ export interface EventRegistrationProps {
 
 export class EventRegistration implements EventRegistrationProps {
     public id: number;
-    public eventRole: EventRole | null = null;
+    public attendance: EventAttendance | null = null;
+    public eventRole: EventRole;
     public "status_id:name": RegistrationStatus;
     public "contact.email_primary.email": string;
 
@@ -26,12 +41,15 @@ export class EventRegistration implements EventRegistrationProps {
         this["status_id:name"] = props["status_id:name"];
         this["contact.email_primary.email"] = props["contact.email_primary.email"];
 
+        const eventAttendanceDetails: Partial<EventAttendanceProps> = {};
         const eventRoleDetails: Partial<EventRoleProps> = {};
         for (const key in props) {
+            if (key.startsWith("attendance.")) eventAttendanceDetails[key.split("attendance.")[1]] = props[key];
             if (key.startsWith("eventRole.")) eventRoleDetails[key.split("eventRole.")[1]] = props[key];
             if (key.startsWith("event.")) eventRoleDetails[key] = props[key];
         }
 
+        this.attendance = eventAttendanceDetails.id ? new EventAttendance(eventAttendanceDetails as EventAttendanceProps) : null;
         this.eventRole = new EventRole(eventRoleDetails as EventRoleProps);
     }
 }
