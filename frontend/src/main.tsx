eef40836f288476b7ac387c0542a75a76425b767
glenn <70140106+ausgore@users.noreply.github.com>
2024-08-14 15:57:08 +0800

    Routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,10 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import "./main.css";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Events from "./pages/v1/Events";
-import Event from "./pages/v1/Event";
+import EventsV1 from "./pages/v1/Events";
+import EventV1 from "./pages/v1/Event";
+import Trainings from "./pages/Trainings";
+import Training from "./pages/Training";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
@@ -13,8 +15,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 			<Routes>
 				<Route index path="/" element={<Home />} />
 				<Route path="/profile" element={<Profile />} />
-				<Route path="/v1/events" element={<Events />} />
-				<Route path="/v1/events/:id" element={<Event />} />
+				<Route path="/v1/events" element={<EventsV1 />} />
+				<Route path="/v1/events/:id" element={<EventV1 />} />
+				<Route path="/trainings" element={<Trainings />} />
+				<Route path="/trainings/:id" element={<Training />} />
 			</Routes>
 		</HashRouter>
 	</React.StrictMode>
