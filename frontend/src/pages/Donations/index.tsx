e11059c93059843140f15744181511b3e48841f9
glenn <70140106+ausgore@users.noreply.github.com>
2024-10-08 13:36:14 +0800

    Total comma and only show completed contributions

--- frontend/src/pages/Donations/index.tsx
+++ frontend/src/pages/Donations/index.tsx
@@ -5,6 +5,7 @@ import { Contribution } from "../../../utils/v2/entities/Contribution";
 import Loading from "../../components/Loading";
 import Summarisation from "./componenets/Summarisation";
 import History from "./componenets/History";
+import numeral from "numeral";
 
 export default function Donations() {
     const email = (window as any).email;
@@ -12,11 +13,11 @@ export default function Donations() {
 
     useEffect(() => {
         (async () => {
-            const donations = await ContributionHandler.fetch(email, [["financial_type_id:label", "NOT IN", ["Campaign Contribtuion", "Event Fees", "Member Dues"]]]);
+            const donations = await ContributionHandler.fetch(email, [["financial_type_id:label", "NOT IN", ["Campaign Contribtuion", "Event Fee", "Member Dues"]], ["contribution_status_id:name", "=", "Completed"]]);
             setDonations(donations);
         })();
     }, []);
-    
+
     return <Wrapper>
         {!donations ? <Loading className="h-screen items-center" /> : <div className="p-4 mb-12">
             <div className="w-full px-0 md:px-6 max-w-[1200px] mx-auto">
@@ -24,7 +25,7 @@ export default function Donations() {
                 <div className="mt-4 bg-white rounded-md p-4 shadow-md">
                     <div className="mb-4">
                         <h2 className="text-2xl font-semibold mb-1">Total Contributions</h2>
-                        <p className="text-2xl text-secondary">S$ {donations.reduce((a, b) => a + b.data.total_amount!, 0).toFixed(2)}</p>
+                        <p className="text-2xl text-secondary">S$ {numeral(donations.reduce((a, b) => a + b.data.total_amount!, 0)).format('0,0.00')}</p>
                     </div>
                     <Summarisation donations={donations} />
                 </div>
