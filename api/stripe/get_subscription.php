dada06c503e76a27f6d51c5b4020c1faaeffcc38
glenn <70140106+ausgore@users.noreply.github.com>
2024-10-10 15:54:48 +0800

    Subscription APIs

---
+++ api/stripe/get_subscription.php
@@ -0,0 +1,29 @@
+<?php
+require '../../vendor/autoload.php';
+
+// Getting the API key from the config.json and instantiating a new StripeClient
+$json = file_get_contents("../../config.json");
+$data = json_decode($json, true);
+$key = $data['stripe_secret_key'];
+$stripe = new \Stripe\StripeClient($key);
+
+// Grabbing the subscriptionId from Post Query
+$query = array();
+if (strpos($_SERVER['CONTENT_TYPE'], 'application/json') !== false) {
+    $query = json_decode(file_get_contents("php://input"), true);
+} else {
+    $query = $_POST;
+}
+
+// Cancelling the subscription with the provided query
+try {
+    $subscriptionId = $query['subscriptionId'];
+    $subscription = $stripe -> subscriptions -> retrieve($subscriptionId);
+
+    http_response_code(200);
+    echo json_encode($subscription);
+}
+catch (\Stripe\Exception\ApiErrorException $e) {
+    http_response_code(400);
+    echo json_encode(['error' => $e -> getMessage()]);
+}
