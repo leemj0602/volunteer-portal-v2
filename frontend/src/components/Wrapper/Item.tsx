c5a98d5d65224821983105f562e6c86020ade034
glenn <70140106+ausgore@users.noreply.github.com>
2024-11-20 17:26:41 +0800

    Item

---
+++ frontend/src/components/Wrapper/Item.tsx
@@ -0,0 +1,17 @@
+import { IconType } from "react-icons"
+import { Link, useLocation } from "react-router-dom";
+
+interface ItemProps {
+  to: string;
+  icon: IconType;
+  name: string;
+}
+
+export default function Item(props: ItemProps) {
+  const location = useLocation();
+
+  return <Link to={props.to} className={`flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold ${location.pathname == props.to ? "bg-primary/20 text-secondary/90 border-l-secondary/70" : "hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70"}`}>
+    <props.icon />
+    <span>{props.name}</span>
+  </Link>
+}
