8ff3df372996ffff83521a2bfe170fb427880edc
glenn <70140106+ausgore@users.noreply.github.com>
2024-11-27 15:48:45 +0800

    Routes

--- frontend/src/components/Wrapper/Category.tsx
+++ frontend/src/components/Wrapper/Category.tsx
@@ -8,19 +8,21 @@ interface CategoryProps extends PropsWithChildren {
   to: string;
   icon?: IconType;
   name: string;
+  location?: string;
 }
 export default function Category(props: CategoryProps) {
   const location = useLocation();
+  const path = props.location ?? location.pathname;
 
-  return <div className={`border-b-2 ${location.pathname.startsWith(props.to) ? " border-primary/20" : "border-transparent"}`}>
-    <Link to={props.to} className={`flex justify-between items-center px-6 py-2 text-secondary ${location.pathname.startsWith(props.to) ? "border-b-[3px] border-b-primary" : "hover:bg-primary/10"}`}>
+  return <div className={`border-b-2 ${path.startsWith(props.to) ? " border-primary/20" : "border-transparent"}`}>
+    <Link to={props.to} className={`flex justify-between items-center px-6 py-2 text-secondary ${path.startsWith(props.to) ? "border-b-[3px] border-b-primary" : "hover:bg-primary/10"}`}>
     <div className="flex items-center gap-x-4">
       {props.icon && <props.icon />}
       <span className="font-semibold">{props.name}</span>
     </div>
-      {location.pathname.startsWith(props.to) ? <FaChevronDown /> : <FaChevronLeft />}
+      {path.startsWith(props.to) ? <FaChevronDown /> : <FaChevronLeft />}
     </Link>
-    {location.pathname.startsWith(props.to) && <div>
+    {path.startsWith(props.to) && <div>
       <Item to={props.to} name="Dashboard" />
       {props.children}
     </div>}
