4358aba0732ed895f4dc3bd0dd1e02cc773788d3
glenn <70140106+ausgore@users.noreply.github.com>
2024-12-23 15:54:17 +0800

    Unnavigable categories via mobile

--- frontend/src/components/Wrapper/Category.tsx
+++ frontend/src/components/Wrapper/Category.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import { IconType } from "react-icons";
 import { FaChevronDown, FaChevronLeft } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
@@ -9,22 +9,38 @@ interface CategoryProps extends PropsWithChildren {
   icon?: IconType;
   name: string;
   location?: string;
+  disableNavigate?: boolean;
 }
 export default function Category(props: CategoryProps) {
   const location = useLocation();
   const path = props.location ?? location.pathname;
+  const [active, setActive] = useState(false);
 
-  return <div className={`border-b-2 ${path.startsWith(props.to) ? " border-primary/20" : "border-transparent"}`}>
-    <Link to={props.to} className={`flex justify-between items-center px-6 py-2 text-secondary ${path.startsWith(props.to) ? "border-b-[3px] border-b-primary" : "hover:bg-primary/10"}`}>
-    <div className="flex items-center gap-x-4">
-      {props.icon && <props.icon />}
-      <span className="font-semibold">{props.name}</span>
-    </div>
-      {path.startsWith(props.to) ? <FaChevronDown /> : <FaChevronLeft />}
-    </Link>
-    {path.startsWith(props.to) && <div>
-      <Item to={props.to} name="Dashboard" />
-      {props.children}
-    </div>}
+  return <div className={`border-b-2 ${(path.startsWith(props.to) || active) ? " border-primary/20" : "border-transparent"}`}>
+    {props.disableNavigate ? <>
+      <div className={`flex justify-between items-center px-6 py-2 text-secondary ${(path.startsWith(props.to) || active) ? 'border-b-[3px] border-b-primary' : 'hover:bg-primary/10'}`} onClick={() => setActive(!active)}>
+        <div className='flex items-center gap-x-4'>
+          {props.icon && <props.icon />}
+          <span className='font-semibold'>{props.name}</span>
+        </div>
+        {(path.startsWith(props.to) || active) ? <FaChevronDown /> : <FaChevronLeft />}
+      </div>
+      {(path.startsWith(props.to) || active) && <div>
+        <Item to={props.to} name="Dashboard" />
+        {props.children}
+      </div>}
+    </> : <>
+      <Link to={props.to} className={`flex justify-between items-center px-6 py-2 text-secondary ${path.startsWith(props.to) ? "border-b-[3px] border-b-primary" : "hover:bg-primary/10"}`}>
+        <div className="flex items-center gap-x-4">
+          {props.icon && <props.icon />}
+          <span className="font-semibold">{props.name}</span>
+        </div>
+        {path.startsWith(props.to) ? <FaChevronDown /> : <FaChevronLeft />}
+      </Link>
+      {path.startsWith(props.to) && <div>
+        <Item to={props.to} name="Dashboard" />
+        {props.children}
+      </div>}
+    </>}
   </div>
 }
--- frontend/src/components/Wrapper/Navbar.tsx
+++ frontend/src/components/Wrapper/Navbar.tsx
@@ -16,130 +16,128 @@ import Item from "./Item";
 import { useSubtypesContext } from "../../contexts/Subtypes";
 
 interface NavbarProps {
-    className: string;
-    location?: string;
+	className: string;
+	location?: string;
 }
 
 export default function Navbar(props: NavbarProps) {
-    const { subtypes } = useSubtypesContext()!;
+	const { subtypes } = useSubtypesContext()!;
 
-    const openModal = async () => {
-        const result = await Swal.fire({
-            imageUrl: SignOut,
-            imageWidth: 240,
-            imageHeight: 160,
-            width: 400,
-            text: "Are you sure you want to sign out?",
-            confirmButtonColor: "#5a71b4",
-            confirmButtonText: "Sign Out",
-            cancelButtonText: "Cancel",
-            showCloseButton: true,
-            showCancelButton: true
-        });
+	const openModal = async () => {
+		const result = await Swal.fire({
+			imageUrl: SignOut,
+			imageWidth: 240,
+			imageHeight: 160,
+			width: 400,
+			text: "Are you sure you want to sign out?",
+			confirmButtonColor: "#5a71b4",
+			confirmButtonText: "Sign Out",
+			cancelButtonText: "Cancel",
+			showCloseButton: true,
+			showCancelButton: true
+		});
 
-        if (result.isConfirmed) {
-            Swal.fire({ title: "Signing out...", timer: 3000, timerProgressBar: true, showConfirmButton: false });
-            await axios.get(`${config.domain}/portal/api/logout.php`);
-            window.location.href = `${config.domain}/wp-login.php?redirect_to=${encodeURIComponent(window.location.href)}`;
-        }
-    }
+		if (result.isConfirmed) {
+			Swal.fire({ title: "Signing out...", timer: 3000, timerProgressBar: true, showConfirmButton: false });
+			await axios.get(`${config.domain}/portal/api/logout.php`);
+			window.location.href = `${config.domain}/wp-login.php?redirect_to=${encodeURIComponent(window.location.href)}`;
+		}
+	}
 
-    const [menuOpen, setMenuOpen] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 
-    return subtypes && <>
-        <nav className={`h-full fixed bg-white flex-col hidden z-[11] overflow-y-auto ${props.className} md:flex`}>
-            {/* Responsible for the image */}
-            <Link to="/" className="p-4 flex items-center">
-                <img src={O8Logo} />
-            </Link>
-            {/* Navigation */}
-            <div className="mt-6 flex flex-col justify-between h-full">
-                <div className="mb-2">
-                    {subtypes.includes('Volunteer') && <Category location={props.location} to="/volunteer" icon={FaBriefcaseMedical} name="Volunteer">
-                        <Item location={props.location} to="/volunteer/events" name="Events" />
-                        <Item location={props.location} to="/volunteer/jobs" name="Jobs" />
-                        <Item location={props.location} to="/volunteer/trainings" name="Trainings" />
-                        <Item to="/volunteer/volunteer-info-pack" name="Volunteer Info Pack" />
-                        <Item to="/volunteer/notifications" name="Notifications" />
-                    </Category>}
-                    {subtypes.includes('Donator') && <Category location={props.location} to="/donor" icon={FaDonate} name="Donations">
-                        <Item to="/donor/campaigns" name="Campaigns" />
-                        <Item to="/donor/donate" name="Donate" />
-                        <Item to="/donor/donor-info-pack" name="Donor Info Pack" />
-                    </Category>}
-                    {subtypes.includes('Caregiver') && <Category location={props.location} to="/caregiver" icon={FaHeartbeat} name="Caregiver">
-                        <Item location={props.location} to="/caregiver/request" name="Request for Volunteer" />
-                        <Item location={props.location} to="/caregiver/trainings" name="Trainings" />
-                        <Item location={props.location} to="/caregiver/service-info-pack" name="Service Info Pack" />
-                    </Category>}
-                    {subtypes.includes('Patient') && <Category location={props.location} to="/patient" icon={FaClinicMedical} name="Patient">
-                        <Item location={props.location} to="/patient/request" name="Request for Volunteer" />
-                        <Item location={props.location} to="/patient/service-info-pack" name="Service Info Pack" />
-                    </Category>}
-                </div>
-                <div>
-                    <Link to="/profile" className="flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70 w-full">
-                        <CgProfile />
-                        <span>Profile</span>
-                    </Link>
-                    <button onClick={openModal} className="flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70 w-full">
-                        <PiSignOutBold />
-                        <span>Sign Out</span>
-                    </button>
-                </div>
-            </div>
-        </nav>
-        {menuOpen && <div className="h-full w-screen bg-black opacity-30 z-[11] fixed md:hidden" onClick={() => setMenuOpen(false)} />}
-        <nav className="w-full fixed bg-white rounded-b-lg flex flex-col z-20 md:hidden">
-            <div className="flex justify-between h-20 items-center px-6">
-                {/* Responsible for the image */}
-                <div className="p-4 w-[220px] flex items-center">
-                    <img src={O8Logo} />
-                </div>
-                {/* Button */}
-                <button className={`text ${menuOpen ? "text-secondary" : "text-primary"} hover:text-secondary`} onClick={() => setMenuOpen(!menuOpen)}>
-                    <GiHamburgerMenu size={24} />
-                </button>
-            </div>
-            {menuOpen && <>
-                <hr />
-                <div className="flex flex-col pt-2 max-h-[200px] overflow-y-scroll">
-                    <Link to="/">
-                        <div className="text-center hover:bg-primary/30 text-secondary hover:text-secondary/90 font-semibold flex pl-12 py-2 mb-2 items-center gap-x-4">
-                            <RxDashboard />
-                            Dashboard
-                        </div>
-                    </Link>
-                    <Link to="/events">
-                        <div className="text-center hover:bg-primary/30 text-secondary hover:text-secondary/90 font-semibold flex pl-12 py-2 mb-2 items-center gap-x-4">
-                            <LuCalendarRange />
-                            Events
-                        </div>
-                    </Link>
-                    <Link to="/trainings">
-                        <div className="hover:bg-primary/30 text-secondary hover:text-secondary/90 font-semibold flex pl-12 py-2 mb-2 items-center gap-x-4">
-                            <FaChalkboardTeacher />
-                            Trainings
-                        </div>
-                    </Link>
-                    <Link to="/profile">
-                        <div className="text-center hover:bg-primary/30 text-secondary hover:text-secondary/90 font-semibold flex pl-12 py-2 mb-2 items-center gap-x-4">
-                            <CgProfile />
-                            Profile
-                        </div>
-                    </Link>
-                    {/* {system?.data.civi?.components.includes("CiviContribute") && <Link to="/donations">
-                        <div className="text-center hover:bg-primary/30 text-secondary hover:text-secondary/90 font-semibold flex pl-12 py-2 mb-2 items-center gap-x-4">
-                            <BiDonateHeart />
-                            Donations
-                        </div>
-                    </Link>} */}
-                    <button onClick={openModal} className="text-center hover:bg-primary/30 text-secondary hover:text-secondary/90 font-semibold flex pl-12 py-2 mb-2 items-center gap-x-4">
-                        <PiSignOutBold />
-                        <span>Sign Out</span>
-                    </button>
-                </div>
-            </>}
-        </nav>
-    </>
+	return subtypes && <>
+		<nav className={`h-full fixed bg-white flex-col hidden z-[11] overflow-y-auto ${props.className} md:flex`}>
+			{/* Responsible for the image */}
+			<Link to="/" className="p-4 flex items-center">
+				<img src={O8Logo} />
+			</Link>
+			{/* Navigation */}
+			<div className="mt-6 flex flex-col justify-between h-full">
+				<div className="mb-2">
+					{subtypes.includes('Volunteer') && <Category location={props.location} to="/volunteer" icon={FaBriefcaseMedical} name="Volunteer">
+						<Item location={props.location} to="/volunteer/events" name="Events" />
+						<Item location={props.location} to="/volunteer/jobs" name="Jobs" />
+						<Item location={props.location} to="/volunteer/trainings" name="Trainings" />
+						<Item to="/volunteer/volunteer-info-pack" name="Volunteer Info Pack" />
+						<Item to="/volunteer/notifications" name="Notifications" />
+					</Category>}
+					{subtypes.includes('Donator') && <Category location={props.location} to="/donor" icon={FaDonate} name="Donations">
+						<Item to="/donor/campaigns" name="Campaigns" />
+						<Item to="/donor/donate" name="Donate" />
+						<Item to="/donor/donor-info-pack" name="Donor Info Pack" />
+					</Category>}
+					{subtypes.includes('Caregiver') && <Category location={props.location} to="/caregiver" icon={FaHeartbeat} name="Caregiver">
+						<Item location={props.location} to="/caregiver/request" name="Request for Volunteer" />
+						<Item location={props.location} to="/caregiver/trainings" name="Trainings" />
+						<Item location={props.location} to="/caregiver/service-info-pack" name="Service Info Pack" />
+					</Category>}
+					{subtypes.includes('Patient') && <Category location={props.location} to="/patient" icon={FaClinicMedical} name="Patient">
+						<Item location={props.location} to="/patient/request" name="Request for Volunteer" />
+						<Item location={props.location} to="/patient/service-info-pack" name="Service Info Pack" />
+					</Category>}
+				</div>
+				<div>
+					<Link to="/profile" className="flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70 w-full">
+						<CgProfile />
+						<span>Profile</span>
+					</Link>
+					<button onClick={openModal} className="flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70 w-full">
+						<PiSignOutBold />
+						<span>Sign Out</span>
+					</button>
+				</div>
+			</div>
+		</nav>
+		{menuOpen && <div className="h-full w-screen bg-black opacity-30 z-[11] fixed md:hidden" onClick={() => setMenuOpen(false)} />}
+		<nav className="w-full fixed bg-white rounded-b-lg flex flex-col z-20 md:hidden">
+			<div className="flex justify-between h-54 items-center px-6">
+				{/* Responsible for the image */}
+				<div className="p-4 w-[220px] flex items-center">
+					<img src={O8Logo} />
+				</div>
+				{/* Button */}
+				<button className={`text ${menuOpen ? "text-secondary" : "text-primary"} hover:text-secondary`} onClick={() => setMenuOpen(!menuOpen)}>
+					<GiHamburgerMenu size={24} />
+				</button>
+			</div>
+			{menuOpen && <>
+				<hr />
+				<div className="flex flex-col pt-2 max-h-[700px] overflow-y-auto">
+					{subtypes.includes('Volunteer') && <Category disableNavigate location={props.location} to="/volunteer" icon={FaBriefcaseMedical} name="Volunteer">
+						<Item location={props.location} to="/volunteer/events" name="Events" />
+						<Item location={props.location} to="/volunteer/jobs" name="Jobs" />
+						<Item location={props.location} to="/volunteer/trainings" name="Trainings" />
+						<Item to="/volunteer/volunteer-info-pack" name="Volunteer Info Pack" />
+						<Item to="/volunteer/notifications" name="Notifications" />
+					</Category>}
+					{subtypes.includes('Donator') && <Category disableNavigate location={props.location} to="/donor" icon={FaDonate} name="Donations">
+						<Item to="/donor/campaigns" name="Campaigns" />
+						<Item to="/donor/donate" name="Donate" />
+						<Item to="/donor/donor-info-pack" name="Donor Info Pack" />
+					</Category>}
+					{subtypes.includes('Caregiver') && <Category disableNavigate location={props.location} to="/caregiver" icon={FaHeartbeat} name="Caregiver">
+						<Item location={props.location} to="/caregiver/request" name="Request for Volunteer" />
+						<Item location={props.location} to="/caregiver/trainings" name="Trainings" />
+						<Item location={props.location} to="/caregiver/service-info-pack" name="Service Info Pack" />
+					</Category>}
+					{subtypes.includes('Patient') && <Category disableNavigate location={props.location} to="/patient" icon={FaClinicMedical} name="Patient">
+						<Item location={props.location} to="/patient/request" name="Request for Volunteer" />
+						<Item location={props.location} to="/patient/service-info-pack" name="Service Info Pack" />
+					</Category>}
+					<hr className='my-2' />
+					<div>
+						<Link to="/profile" className="flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70 w-full">
+							<CgProfile />
+							<span>Profile</span>
+						</Link>
+						<button onClick={openModal} className="flex pl-9 py-2 items-center gap-x-4 text-secondary border-l-4 font-semibold hover:bg-primary/10 hover:text-secondary/90 border-l-transparent hover:border-l-secondary/70 w-full">
+							<PiSignOutBold />
+							<span>Sign Out</span>
+						</button>
+					</div>
+				</div>
+			</>}
+		</nav>
+	</>
 }
